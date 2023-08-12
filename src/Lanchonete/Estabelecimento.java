1324bae346dc7ea55523b05b16b6012cdef014c6
leiah olive <liaanjos87@gmail.com>
2023-08-12 12:57:41 -0300

    first commit

---
+++ src/Lanchonete/Estabelecimento.java
@@ -0,0 +1,38 @@
+package Lanchonete;
+
+import Lanchonete.area.cliente.Cliente;
+import Lanchonete.atendimento.Atendente;
+import Lanchonete.atendimento.cozinha.Almoxarife;
+import Lanchonete.atendimento.cozinha.Cozinheiro;
+
+public class Estabelecimento {
+    public static void main(String[] args) {
+        Cozinheiro cozinheiro = new Cozinheiro();
+
+
+        //ações que estabelecimento precisa ter ciência
+        cozinheiro.adicionarSucoNoBalcao();;
+        cozinheiro.adicionarLancheNoBalcao();
+        cozinheiro.adicionarComboNoBalcao();
+
+        Almoxarife almoxarife = new Almoxarife();
+
+
+
+        Atendente atendente = new Atendente();
+        atendente.pegarLancheCozinha();
+        atendente.receberPagamento();
+        atendente.servindoMesa();
+
+
+        Cliente cliente = new Cliente();
+        cliente.escolherLanche();
+        cliente.fazerPedido();
+        cliente.pagarConta();
+
+
+
+
+
+    }
+}
